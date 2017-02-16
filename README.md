49524649c1736fdc36784f47db85dd68f99e2b78
Richard Strauss <rstrauss91@gmail.com>
2017-02-16 16:13:51 -0500

    finish assignment

--- README.md
+++ README.md
@@ -1,3 +1,38 @@
 # Your First (Several) Asynchronous Requests
 
 [See assignment in Alexa.](https://alexa.bitmaker.co/cohorts/67/assignments/2055/latest)
+
+
+Step 1:
+Open the index.html file from the cloned project in your browser.
+
+In the linked ajax.js file, build an AJAX request that:
+retrieves the information at the root path of the server, by sending an empty JavaScript object, and expecting a text response.
+Ensure the request is wrapped in a $(document).ready.
+Ensure your Network Tab is open in Developer tools, and run the request by reloading your page.
+
+$(document).ready(function () {
+  $.ajax({
+  url: 'http://first-ajax-api.herokuapp.com/',
+  method: 'GET',
+  dataType: 'text'
+})
+})
+What are the values in the "Method" and "Status" columns? Compare these values to Step 0.
+Method: GET
+Status: 200 OK
+ -> The same as in Step 0 when I went directly to the root page of the server directly.
+What are the values in the "Type" and "Initiator" columns? Compare these values to Step 0.
+ ->type: xhr;
+ ->initiator: jquery.min.js:4;
+
+Step 2:
+ $(document).ready(function () {
+ $('#s12').on('click',function() {
+   $.ajax({
+   url: 'http://first-ajax-api.herokuapp.com/',
+   method: 'GET',
+   dataType: 'text'
+   })
+ })
+ })
--- ajax.js
+++ ajax.js
@@ -1,5 +1,57 @@
 $(document).ready(function () {
 
-  /* Your code goes here */
+$('#s12').on('click',function() {
+  $.ajax({
+  url: 'http://first-ajax-api.herokuapp.com/',
+  method: 'GET',
+  dataType: 'text'
+  })
+})
 
-});
+$('#s3456').on('click',function() {
+  $.ajax({
+  url: 'http://first-ajax-api.herokuapp.com/ping',
+  method: 'GET',
+  dataType: 'text'
+}).done(function(data){
+  $('#step3456').append(data);
+  console.log("Success!")
+}).fail(function(data){
+console.log(data.responseText);
+}).always(function(){
+  console.log("The response is complete")
+})
+})
+
+$('#s7').on('click',function() {
+  $.ajax({
+    url: 'http://first-ajax-api.herokuapp.com/count',
+    method: 'GET',
+    dataType: 'text'
+  }).done(function(data) {
+    $('#step7').append(data);
+  })
+})
+
+$('#s8').on('click',function() {
+  $.ajax({
+    url: 'http://first-ajax-api.herokuapp.com/time',
+    method: 'GET',
+    data: {timezone: 'Europe/Sofia'},
+    dataType: 'text'
+  }).done(function(data){
+    $('#step8').append(data);
+  })
+})
+
+$('#s9').on('click',function() {
+  $.ajax({
+    url: 'http://first-ajax-api.herokuapp.com/a_car',
+    method: 'GET',
+    dataType: 'html'
+  }).done(function(data){
+    $('#ss9').append(data);
+  })
+})
+
+})
