d15b819e2d92eaec88dc27bf93c3fca6f72fbe48
Richard Strauss <rstrauss91@gmail.com>
2017-02-17 14:12:47 -0500

    add additional pop-up functionality

--- ajax.js
+++ ajax.js
@@ -27,9 +27,12 @@ $('#s7').on('click',function() {
   $.ajax({
     url: 'http://first-ajax-api.herokuapp.com/count',
     method: 'GET',
+    data: {amount: 6},
     dataType: 'text'
   }).done(function(data) {
-    $('#step7').append(data);
+    $('#step7').append(data+"<br>");
+  }).fail(function(jqXHR,textStatus,errorThrown) {
+    console.log(jqXHR);
   })
 })
 
@@ -43,6 +46,30 @@ $('#s8').on('click',function() {
     $('#step8').append(data);
   })
 })
+
+
+$('#s8b').on('click',function() {
+  // var t = prompt("What timezone are you looking for?")
+  $("<textarea id='s8c'> </textarea").appendTo($('#step8'));
+  $("<button id='s8d'>Submit</button>").appendTo($('#step8'));
+})
+
+$('#step8').on('click','#s8b',function() {
+  alert("It's work!")
+  $('#s8d').on('click',function() {
+    $.ajax({
+      url: 'http://first-ajax-api.herokuapp.com/time',
+      method: 'GET',
+      data: {timezone: $('#s8c').val()},
+      dataType: 'text'
+    }).done(function(data){
+      $('#step8').append(data);
+    }).always(function(){
+      console.log("Request complete")
+    })
+})
+})
+
 
 $('#s9').on('click',function() {
   $.ajax({
